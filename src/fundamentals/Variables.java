8480671bac476895d4a9d6df6a4ddba37b196864
Laura Barauna <lauravbarauna@gmail.com>
2025-02-11 17:53:05 -0300

    Finish Java fundamentals lesson covering variables, data types, and initialization

---
+++ src/fundamentals/Variables.java
@@ -0,0 +1,23 @@
+package fundamentals;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int x; //declaration
+        x = 123; //assignment
+        int y = 123; //initialization
+
+        long z = 12312312312323L;
+        double a = 3.14;
+        float b = 3.14f;
+        boolean c = true;
+        char symbol = '@';
+        String name = "Laura";
+
+        System.out.println("My number is: "+a);
+        System.out.println(b);
+        System.out.println(symbol);
+        System.out.println("My name is: "+name);
+
+    }
+}
