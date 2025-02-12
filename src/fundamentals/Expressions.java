6d644dababd4deac1597585563ae41792d74042c
Laura Barauna <lauravbarauna@gmail.com>
2025-02-12 08:53:33 -0300

    Finish Java fundamentals lesson on expressions and operators

---
+++ src/fundamentals/Expressions.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Expressions {
+    public static void main (String[] args) {
+        // Expression = operands & operators
+        // Operands = values, variables, numbers, quantity
+        // Operators = + - * / %
+
+        int friends = 10;
+
+        // When you divide an int value, the result will be an integer, so 10 / 3 = 3, not 3.3333...
+        friends = friends / 3;
+
+        System.out.println(friends);
+
+    }
+}
