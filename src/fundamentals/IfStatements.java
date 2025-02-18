0bebe5ab0be5a4a38d1f411dd8ecc6280d75c92d
Laura Barauna <lauravbarauna@gmail.com>
2025-02-18 19:08:38 -0300

    Finish Java lesson on if statements for conditional logic

---
+++ src/fundamentals/IfStatements.java
@@ -0,0 +1,19 @@
+package fundamentals;
+
+public class IfStatements {
+
+    public static void main(String[] args) {
+
+        int age = 75;
+
+        if(age == 75) {
+            System.out.println("You are older than 75!");
+        } else if (age >= 18) {
+            System.out.println("You are older than 18!");
+        } else {
+            System.out.println("You are not older than 18!");
+        }
+
+    }
+
+}
